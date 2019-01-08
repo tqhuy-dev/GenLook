f7bab03b8ea86e202a45f88eb49f74efd8b29482
HuyTran1996 <tqhuy1996.developer@gmail.com>
2019-01-08 20:43:43 +0700

    add list-location , location component

--- GenLook/src/app/Main/main.module.ts
+++ GenLook/src/app/Main/main.module.ts
@@ -3,8 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { MainComponent } from './main/main.component';
 import { CarouselComponent } from '../Main/carousel/carousel.component';
+import { LocationComponent } from '../Main/location/location.component';
+import { ListLocationComponent } from '../Main/list-location/list-location.component';
 @NgModule({
-    declarations: [ToolBarComponent, MainComponent, CarouselComponent],
+    declarations: [
+        ToolBarComponent,
+        MainComponent,
+        CarouselComponent,
+        LocationComponent,
+        ListLocationComponent],
     imports: [ CommonModule  ],
     exports: [MainComponent],
     providers: [],
